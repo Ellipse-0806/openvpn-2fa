eaf58e74d3fc82fc1bcf7218ff37e756c57fbdb9
Ellipse-0806 <koin.pokken@gmail.com>
2023-07-18 00:00:39 +0900

    first commit

---
+++ softether/setting/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+vpnserver start
+while ! pgrep vpnserver >/dev/null; do sleep 1; done
+sleep 10
+vpncmd localhost /SERVER /IN:/root/setting/softether-setting.txt
+while true; do sleep 1000; done
