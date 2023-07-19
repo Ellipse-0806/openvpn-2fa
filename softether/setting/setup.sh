e9fb22df93ff590abb4cecc4f7ce00fba8b57804
Ellipse-0806 <koin.pokken@gmail.com>
2023-07-20 01:26:23 +0900

    Changed setting file path.

--- softether/setting/setup.sh
+++ softether/setting/setup.sh
@@ -2,5 +2,5 @@
 vpnserver start
 while ! pgrep vpnserver >/dev/null; do sleep 1; done
 sleep 10
-vpncmd localhost /SERVER /IN:/root/setting/softether-setting.txt
+vpncmd localhost /SERVER /IN:/root/setting/softether-setting.ini
 while true; do sleep 1000; done
